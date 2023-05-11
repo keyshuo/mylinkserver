c81b535e76a1ffb10164a3dc902e0bbea6915635
王骁 <614481987@qq.com>
2023-05-10 23:20:19 -0700

    test

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/keyshuo/mylinkserver@v0.0.0-20230511054547-d51e401c411e"
-)
+import "MyLink_Server/server/internal/app"
 
 func main() {
 	serv := app.NewServer()
